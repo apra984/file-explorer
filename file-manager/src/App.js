9bf71f25b675fe128d1c9b134b8939f092562303
Akash Prakash <apra984@aucklanduni.ac.nz>
2021-03-02 16:10:44 +1300

    Add folder/file components nested

--- file-manager/src/App.js
+++ file-manager/src/App.js
@@ -1,6 +1,27 @@
+import Folder from './Folder';
+import File from './File';
+
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div>
+      <Folder name="Desktop" isOpen={false}>
+        <Folder name="Music">
+          <File name="beats.wav" />
+          <File name="sounds.mp3" />
+        </Folder>
+        <Folder name="Videos">
+          <File name="bananas.mp4" />
+          <File name="monkeys.mp4" />
+        </Folder>
+        <File name="biscuits.png" />
+        <File name="notes.docx" />
+      </Folder>
+
+      <Folder name="Applications">
+        <File name="Word.exe" />
+        <File name="Chrome.exe" />
+      </Folder>
+    </div>
   );
 }
 
